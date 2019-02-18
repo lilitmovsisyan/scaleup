c5638761fd041fd9e82117d51a465206515dcd41
Lilit Movsisyan <lilit.momo@gmail.com>
2019-02-18 20:26:23 +0000

    (squashed in error: 1. try to get static files, not working 2. implement bootstrap card layout)

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,3 +23,6 @@ urlpatterns = [
     path('scaleup', include('scaleup.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
