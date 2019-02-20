3443df94fc2d1371487e1142e9fb686d6e38b836
Lilit Movsisyan <lilit.momo@gmail.com>
2019-02-20 16:01:06 +0000

    change url name to ensure reusability

--- scaleup/urls.py
+++ scaleup/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='scaleup'),
     path('newscale/', views.newscale, name='newscale'),
     path('newscale/<str:user>/', views.nextscale, name='nextscale'),
 ]
